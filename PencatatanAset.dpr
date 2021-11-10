c1175635dce9167a28bf008c9243c0a8dd32e5ee
Noorwahid717 <noorwahid052002@gmail.com>
2021-11-10 21:25:27 +0700

    rename aplikasi

---
+++ PencatatanAset.dpr
@@ -0,0 +1,14 @@
+program PencatatanAset;
+
+uses
+  Vcl.Forms,
+  UMAin in 'UMAin.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  AApplication.CreateForm(TForm1, Form1);
+  pplication.Run;
+end.
